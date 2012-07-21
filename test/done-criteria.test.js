02ca6a07796efd5184f2f4d9c7c0442128799992
Atsuya Takagi <asoftonight@gmail.com>
2012-07-21 05:29:54 -0700

    implemet named criteria

--- test/done-criteria.test.js
+++ test/done-criteria.test.js
@@ -2,31 +2,68 @@ var helper = require('./support/helper')
   , DoneCriteria = require('../lib/done-criteria');
 
 describe('done-criteria', function() {
-  it('calls callback when all criteria are done', function(done) {
-    var doneCriteria = new DoneCriteria(['unko', 'geri'], done);
-    doneCriteria.done('unko');
-    doneCriteria.done('geri');
+  var error = function() { throw new Error('Callback is called'); };
+
+  describe('with unnamed criteria', function() {
+    it('calls callback when all criteria are done', function(done) {
+      var doneCriteria = new DoneCriteria(['unko', 'geri'], done);
+      doneCriteria.done('unko');
+      doneCriteria.done('geri');
+    });
+    
+    it('does not call callback when criteria are not done', function(done) {
+      var doneCriteria = new DoneCriteria(['unko', 'geri'], error);
+      doneCriteria.done('unko');
+      doneCriteria.done('shikko');
+
+      setTimeout(function() { done(); }, 1500);
+    });
   });
 
-  it('calls callback when specified partial criteria are done', function(done) {
-    var getIngredients = function(name) {
-      name.should.eql('get ingredients');
-    };
-    var cook = function(name) {
-      name.should.eql('cook');
-    };
-    var doneCriteria = new DoneCriteria(
-        {
-            'get ingredients': [['egg', 'flour', 'milk'], getIngredients]
-          , 'cook': [['some step', 'that i dont know'], cook]
-        }
-      , done
-    );
+  describe('with named criteria', function() {
+    it('calls callback when all criteria are done', function(done) {
+      var getIngredients = function(name) {
+        name.should.eql('get ingredients');
+      };
+      var cook = function(name) {
+        name.should.eql('cook');
+      };
+      var doneCriteria = new DoneCriteria(
+          {
+              'get ingredients': [['egg', 'flour', 'milk'], getIngredients]
+            , 'cook': [['some step', 'that i dont know'], cook]
+          }
+        , done
+      );
 
-    doneCriteria.done('get ingredients', 'egg');
-    doneCriteria.done('cook', 'that i dont know');
-    doneCriteria.done('get ingredients', 'milk');
-    doneCriteria.done('get ingredients', 'flour');
-    doneCriteria.done('cook', 'some step');
+      doneCriteria.done('get ingredients', 'egg');
+      doneCriteria.done('cook', 'that i dont know');
+      doneCriteria.done('get ingredients', 'milk');
+      doneCriteria.done('get ingredients', 'flour');
+      doneCriteria.done('cook', 'some step');
+    });
+
+    it('does not call when criteria are not done', function(done) {
+      var getIngredients = function(name) {
+        name.should.eql('get ingredients');
+      };
+      var cook = function(name) {
+        throw new Error('Callback is called');
+      };
+      var doneCriteria = new DoneCriteria(
+          {
+              'get ingredients': [['egg', 'flour', 'milk'], getIngredients]
+            , 'cook': [['some step', 'that i dont know'], cook]
+          }
+        , error
+      );
+
+      doneCriteria.done('get ingredients', 'egg');
+      doneCriteria.done('cook', 'that i dont know');
+      doneCriteria.done('get ingredients', 'milk');
+      doneCriteria.done('get ingredients', 'flour');
+      
+      setTimeout(function() { done(); }, 1500);
+    });
   });
 });
